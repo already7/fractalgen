7af75b5e6572ac72dcfc72044926a7f324d51947
already7 <dannilassen1@mail.ru>
2025-04-22 22:58:33 +0300

    tests

--- demo/run.py
+++ demo/run.py
@@ -33,79 +33,13 @@ def compare_images(image1_path, image2_path):
     arr2 = np.array(img2)
     
     equal_pixels = np.all(arr1 == arr2, axis=-1)
-    percentage = (np.sum(equal_pixels) * 1.0 / equal_pixels.size) * 100
-    return percentage
+    print("Количество совпавших пикселей:", np.sum(equal_pixels), "Количество пикселей:", equal_pixels.size)
+    diff = np.abs(arr1.astype(np.int16) - arr2.astype(np.int16))
+    total_diff = np.sum(diff)
+    print("Суммарная разница между пикселями", total_diff)
 
 
-print(f"{compare_images('sdocker.png', 'samples.png'):.3f}")
-
-# model_type = "fractalmar_in64" #@param ["fractalar_in64", "fractalmar_in64"]
-# if model_type == "fractalar_in64":
-#   download.download_pretrained_fractalar_in64(overwrite=False)
-#   num_conds=1
-# elif model_type == "fractalmar_in64":
-#   download.download_pretrained_fractalmar_in64(overwrite=False)
-#   num_conds=5
-# else:
-#   raise NotImplementedError
-# model = fractalgen.__dict__[model_type](
-#     guiding_pixel=False,
-#     num_conds=num_conds,
-# ).to(device)
-# state_dict = torch.load("pretrained_models/{}/checkpoint-last.pth".format(model_type))["model"]
-# model.load_state_dict(state_dict)
-# model.eval() # important!
-
-# # Set user inputs
-# seed = 0 #@param {type:"number"}
-# torch.manual_seed(seed)
-# np.random.seed(seed)
-# class_labels = 207, 360 #@param {type:"raw"}
-# num_iter_list = 64, 16 #@param {type:"raw"}
-# cfg_scale = 5 #@param {type:"slider", min:1, max:10, step:0.5}
-# cfg_schedule = "constant" #@param ["linear", "constant"]
-# temperature = 1.02 #@param {type:"slider", min:0.9, max:1.2, step:0.01}
-# filter_threshold = 1e-4
-# samples_per_row = 1 #@param {type:"number"}
-
-# label_gen = torch.Tensor(class_labels).long().to(device)
-# class_embedding = model.class_emb(label_gen)
-# if not cfg_scale == 1.0:
-#   class_embedding = torch.cat([class_embedding, model.fake_latent.repeat(label_gen.size(0), 1)], dim=0)
-
-# with torch.no_grad():
-#   with torch.cuda.amp.autocast():
-#     sampled_images = model.sample(
-#       cond_list=[class_embedding for _ in range(num_conds)],
-#       num_iter_list=num_iter_list,
-#       cfg=cfg_scale, cfg_schedule=cfg_schedule,
-#       temperature=temperature,
-#       filter_threshold=filter_threshold,
-#       fractal_level=0,
-#       visualize=False)
-
-# # Denormalize images.
-# pix_mean = torch.Tensor([0.485, 0.456, 0.406]).to(device).view(1, -1, 1, 1)
-# pix_std = torch.Tensor([0.229, 0.224, 0.225]).to(device).view(1, -1, 1, 1)
-# sampled_images = sampled_images * pix_std + pix_mean
-# sampled_images = torch.nn.functional.interpolate(sampled_images, scale_factor=4, mode="nearest")
-# sampled_images = sampled_images.detach().cpu()
-
-# # Save & display images
-# save_image(sampled_images, "samples.png", nrow=int(samples_per_row), normalize=True, value_range=(0, 1))
-# #samples = Image.open("samples3.png")
-
-# #from IPython.display import display, clear_output
-# #clear_output(wait=True)
-# #display(samples)
-
-# print(f"{compare_images('sdocker.png', 'samples.png'):.3f}")
-
-
-
-
-
-
+compare_images('sdocker.png', 'samples.png')
 
 model_type = "fractalmar_base_in256" #@param ["fractalmar_base_in256", "fractalmar_large_in256", "fractalmar_huge_in256"]
 num_conds = 5
@@ -162,6 +96,6 @@ sampled_images = sampled_images.detach().cpu()
 # Save & display images
 save_image(sampled_images, "samples.png", nrow=int(samples_per_row), normalize=True, value_range=(0, 1))
 
-print(f"{compare_images('sdocker.png', 'samples.png'):.3f}")
+compare_images('sdocker.png', 'samples.png')
 
 input()
